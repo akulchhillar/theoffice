8ec3be56e23f172a8c153ea2fb4cdbb4cc5810e0
akulchhillar <32826048+akulchhillar@users.noreply.github.com>
2025-01-19 23:20:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ if "selected_char" not in st.session_state:
 
 
 
-def get_response():
+def get_response(query):
     agent = Agent(model=Groq(id="llama-3.3-70b-versatile"),tools=[NewspaperTools()], show_tool_calls=True,
     description="You are going to act as 'Michael_Scott' from the series 'The Office' and you are going to answer all the questions asked by the user. You will answer as if you are him.",
     instructions=["Scrape the url https://theoffice.fandom.com/wiki/Michael_Scott and https://en.wikipedia.org/wiki/Michael_Scott_(The_Office)#Character_details,_arc,_backstory using 'NewspaperTools' from the tool kit and use the content to understand and think like 'Michael Scott'.",
     "Answer the user's questions using the content from the url."])
-    response = agent.run("How is your day going?")
+    response = agent.run(query)
 
     return response.content
 
@@ -39,7 +39,7 @@ def append_data():
     prompt_value = st.session_state.prompt_value
     
     st.session_state['user_messages'].append(prompt_value)
-    st.session_state['bot_messages'].append(get_response())
+    st.session_state['bot_messages'].append(get_response(prompt_value))
     st.session_state['count_messages'] = st.session_state['count_messages'] + 1
 
     for msg in range(0,st.session_state['count_messages']):
